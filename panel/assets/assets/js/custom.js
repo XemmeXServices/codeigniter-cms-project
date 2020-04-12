08f3eb0cdd238fd66d01c419ed5cb3bb92e0f330
Ferhat Demir <dmrfrht1@gmail.com>
2020-04-13 00:19:01 +0300

    sweetalert ile confirm kullanilarak kayit silme islemi gercekletirildi

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,10 +1,24 @@
 $(function () {
 
-  swal({
-    title: 'Error!',
-    text: 'Do you want to continue',
-    type: 'error',
-    confirmButtonText: 'Cool'
-  })
+  $(".remove-btn").on('click', function () {
+    var $data_url = $(this).data("url");
+
+    swal({
+      title: 'Emin misiniz?',
+      text: "Bu işlemi geri alamayacaksınız",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Evet, sil!',
+      cancelButtonText: 'Hayırl'
+    }).then((result) => {
+      if (result.value) {
+        window.location.href = $data_url;
+      }
+    });
+
+  });
+
 
 });
