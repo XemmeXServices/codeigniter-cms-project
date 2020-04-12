fd68f317d01590fe865a82ef2e23762c6b53bb35
Ferhat Demir <dmrfrht1@gmail.com>
2020-04-12 14:10:16 +0300

    win10 yeniden kurulum

---
+++ panel/application/controllers/Product.php
@@ -0,0 +1,54 @@
+<?php
+
+class Product extends CI_Controller
+{
+    public $viewFolder = "";
+
+    public function __construct()
+    {
+
+        parent::__construct();
+
+        $this->viewFolder = "product_v";
+
+        $this->load->model("product_model");
+
+    }
+
+    public function index(){
+
+        $viewData = new stdClass();
+
+        /** Tablodan Verilerin Getirilmesi.. */
+        $items = $this->product_model->get_all();
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "list";
+        $viewData->items = $items;
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
+
+
+    public function new_form(){
+
+        $viewData = new stdClass();
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "add";
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+
+    }
+
+
+    public function save(){
+
+        echo "saved";
+
+    }
+
+
+}
